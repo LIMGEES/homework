de65366ec58033f7df3f38142e99328440230881
LIMGEES <149366415+LIMGEES@users.noreply.github.com>
2024-01-16 17:33:20 +0900

    Add files via upload
    uploads 0116

---
+++ sbb/src/test/java/com/example/sbb/SbbApplicationTests2.java
@@ -0,0 +1,27 @@
+package com.example.sbb;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbbApplicationTests2 {
+	@Autowired
+    private QuestionRepository questionRepository;
+	
+	@Test
+	void testJpa() {
+		 List<Question> all = this.questionRepository.findAll();
+	        assertEquals(2, all.size());
+
+	        Question q = all.get(0);
+	        assertEquals("sbb가 무엇인가요?", q.getSubject());
+	        for(int i=0;i<all.size();i++)
+	        	System.out.println(all.get(i).toString());
+	}
+
+}
